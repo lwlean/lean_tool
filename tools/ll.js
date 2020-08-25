23de2dcb7862ce7c0739bc2ea2fd9aaeda5e5d83
lwlean <lwlean@163.com>
2020-08-26 00:44:06 +0800

    add black list

--- tools/ll.js
+++ tools/ll.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 const args = require('yargs').argv;
 const colors = require('colors');
 
+
 var dirArr = [];
 var fileArr = [];
 
 function list_dic() {
+    var blackFile=  require('../config/BlackFile.json');
     const findName = args.f;
+    var err_name = '';
+    var blackFileArr = blackFile['black_file'];
+    console.log('blackfile:' + blackFileArr);
     try {
         // 增加读取参数
         var currentDic = './';
@@ -19,9 +24,14 @@ function list_dic() {
             }
             return dirs;
         }).forEach(d => {
+            if ( d !== '' && blackFileArr.indexOf(d.trim()) > -1 ) {
+                return;
+            }
+            err_name = d.trim();
             if (findName !== undefined && d.trim().indexOf(findName) < 0) {
                 return;
             }
+            
             let path = currentDic + '/' + d;
             let stat = fs.statSync(path, (err, stat) => {
                 if (err) {
@@ -55,16 +65,20 @@ function list_dic() {
 
         dirArr.length = fileArr.length = 0; // 清空数组
     } catch(e) {
+        console.error('err name directory or file:' + err_name);
         console.error('catch err:', e);
-    }
-}
-
-function getFileStats(path) {
-    try {
-         
-    } catch (e) {
-        console.err('catch err path:', path);
-        return null;
+        blackFileArr.push(err_name);
+        var blackFile = {
+            "black_file": blackFileArr
+        }
+        let jsonstr = JSON.stringify(blackFile);
+        fs.writeFileSync(`${__dirname}/../config/BlackFile.json`, jsonstr, function(err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('写入黑名单成功：'+err_name);
+            }
+        });
     }
 }
 
