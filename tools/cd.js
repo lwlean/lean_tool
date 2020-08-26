f439a70b991e2aeddc257858893c5853d332e25a
lwlean <lwlean@163.com>
2020-08-26 16:24:23 +0800

    give up cd cli

--- tools/cd.js
+++ tools/cd.js
@@ -1,23 +1,7 @@
 #!node
-const { exec } = require('child_process');
-const args = require('yargs').argv;
 
 function cd() {
-    let arg = process.argv[2];
-    console.log(arg);
-    exec(`cd ${arg}`, { cwd : './' }, (err, stdout, stderr) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        exec(`ll`, (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log(stdout)
-        });
-    });
+    console.log('-- empty --');
 }
 
 cd();
