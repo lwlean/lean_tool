008a605b80f023534ccf013be6b1ef30782793a4
lwlean <lwlean@163.com>
2020-09-02 17:40:51 +0800

    add websearch

---
+++ util/websearch.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process');
+
+const searchDic = {
+    zhihu: 'https://www.zhihu.com/search?q=',
+    baidu: 'https://www.baidu.com/s?wd=',
+    weibo: 'https://s.weibo.com/weibo?q=',
+    npmjs: 'https://www.npmjs.com/search?q='
+}
+
+const search = (site, param) => {
+    if (param === undefined || param === '' || param === null) return;
+    if (process.platform !== 'win32') return;
+    let url = searchDic[site];
+    if (url === undefined || url === null) return;
+    exec('chrome ' + url + param, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(stdout);
+    });
+} 
+
+module.exports = search;
